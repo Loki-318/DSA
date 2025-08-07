8435c943f259790bcd75a7c462e07ed7f88ef8ff
Lokesh HM <122295572+Loki-318@users.noreply.github.com>
2025-08-07 11:16:52 +0530

    Updated StringRotationsOfEachOther.cpp

--- Strings/StringRotationsOfEachOther.cpp
+++ Strings/StringRotationsOfEachOther.cpp
@@ -16,3 +16,11 @@ class Solution {
         return false;
     }
 };
+
+// Optimal O(n) solution
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        return (s1.size() == s2.size() && (s2 + s2).find(s1) != string::npos);
+    }
+};
