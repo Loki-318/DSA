0bb66a804ad76689e6a55318b99daa52d263246b
Lokesh HM <122295572+Loki-318@users.noreply.github.com>
2025-08-07 10:24:15 +0530

    Created StringRotationsOfEachOther.cpp

---
+++ Strings/StringRotationsOfEachOther.cpp
@@ -0,0 +1,18 @@
+// O(n^2) solution
+
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        // code here
+        if(s1 == s2) return true;
+        string res = s2;
+        int n = s2.size();
+        for(int i = n - 1; i > 0; i--){
+            res = s2[i] + res;
+            res.erase(res.size() - 1);
+            
+            if(res == s1) return true;
+        }        
+        return false;
+    }
+};
